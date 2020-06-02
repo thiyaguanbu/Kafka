6701e793d736ded842389b75c2a5a0229c00ec49
thiyagu.anbu <thethiyagu25@gmail.com>
2020-06-02 16:17:04 +0530

    first commit

---
+++ mykakfaproject/src/test/java/com/example/mykakfaproject/MykakfaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mykakfaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykakfaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
